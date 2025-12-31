12d863f86dacc453743013619fafda5215a782a1
mahmoudzenbarakji <mahmmoud.zenbarakji@gmail.com>
2025-12-31 21:58:22 +0300

    m

--- resources/themes/default/layouts/front-end/partials/_header.blade.php
+++ resources/themes/default/layouts/front-end/partials/_header.blade.php
@@ -1,6 +1,8 @@
-@php($announcement = getWebConfig(name: 'announcement'))
+@php
+    $announcement = getWebConfig('announcement');
+@endphp
 
-@if (isset($announcement) && $announcement['status'] == 1)
+@if (!empty($announcement) && $announcement['status'] == 1)
     <div class="text-center position-relative px-4 py-1" id="announcement"
          style="background-color: {{ $announcement['color'] }}; color: {{ $announcement['text_color'] }}">
         <span>{{ $announcement['announcement'] }}</span>
@@ -11,21 +13,19 @@
 <header class="rtl __inline-10">
     <div class="topbar">
         <div class="container">
+
             <div>
                 <div class="topbar-text dropdown d-md-none ms-auto">
                     <a class="topbar-link direction-ltr" href="tel:{{ $web_config['phone']->value }}">
                         <i class="fa fa-phone"></i> {{ $web_config['phone']->value }}
                     </a>
                 </div>
-                <div class="d-none d-md-block mr-2 text-nowrap">
-                    <a class="topbar-link d-none d-md-inline-block direction-ltr" href="tel:{{ $web_config['phone']->value }}">
-                        <i class="fa fa-phone"></i> {{ $web_config['phone']->value }}
-                    </a>
-                </div>
             </div>
 
             <div>
-                @php($currency_model = getWebConfig(name: 'currency_model'))
+                @php
+                    $currency_model = getWebConfig('currency_model');
+                @endphp
 
                 @if ($currency_model === 'multi_currency')
                     <div class="topbar-text dropdown disable-autohide mr-4">
@@ -33,8 +33,18 @@
                             <span>{{ session('currency_code') }} {{ session('currency_symbol') }}</span>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-{{ Session::get('direction') === 'rtl' ? 'right' : 'left' }}">
-                            @foreach (\App\Models\Currency::where('status', 1)->get() as $currency)
-                                <li class="dropdown-item cursor-pointer get-currency-change-function"
+                            @php
+                                $currencies = collect([]);
+                                try {
+                                    if (\Illuminate\Support\Facades\Schema::hasTable('currencies')) {
+                                        $currencies = \App\Models\Currency::where('status', 1)->get();
+                                    }
+                                } catch (\Exception $e) {
+                                    $currencies = collect([]);
+                                }
+                            @endphp
+                            @foreach ($currencies as $currency)
+                                <li class="dropdown-item cursor-pointer"
                                     data-code="{{ $currency->code }}">
                                     {{ $currency->name }}
                                 </li>
@@ -43,15 +53,13 @@
                     </div>
                 @endif
             </div>
+
         </div>
     </div>
 
-    {{-- ================= FIX VARIABLE ================= --}}
     @php
         $arrowDirection = Session::get('direction') === 'rtl' ? 'left' : 'right';
     @endphp
-    {{-- ================================================= --}}
-
 </header>
 
 @push('script')
